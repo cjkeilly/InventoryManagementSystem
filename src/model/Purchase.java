8cffa613888ce96719fccb2e0823fe54313a4852
summersnyder <summersnyder@localhost>
2021-11-29 20:05:53 -0500

    Summer

--- src/model/Purchase.java
+++ src/model/Purchase.java
@@ -1,6 +1,35 @@
 package model;
 
 public class Purchase {
+	
     private Item item;
     private int numUnits;
+    private Connection con;
+    private double tax;
+    private double pricePerUnit;
+    private double taxCost;
+    
+    
+    private  ArrayList<Purchase> purchases;
+    
+    // method for calculating tax
+    public void calculateTax(double tax, double pricePerUnit, int numUnits) {
+		double taxCost = (pricePerUnit * numUnits) * tax;
+	}
+	
+    // method for calculating the total, after tax
+	public void calculateTotal(double pricePerUnit, double taxCost, int numUnits) {
+		double total = (pricePerUnit * numUnits) + taxCost;
+	}
+
+	// getter method for selectedItem
+	public void getSelectedItem() {
+		return selectedItem;
+	}
+	
+	// setter method for selectedItems
+	public void setSelectedItems(Item selectedItem) {
+		this.selectedItem = selectedItem;
+	}
+
 }
--- src/model/Sale.java
+++ src/model/Sale.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class Sale {
 
     private ArrayList<Item> cart;
+    
+	// method to display the items purchased?
+	public void displayItems(Item item) {
+		
+	}
+
+	// method to recieve a notification that your items have been purchased?
+	public void notification() {
+
+	}
+}
 }
